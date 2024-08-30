f9443abda3c4e5d9907fe9960495fb021c55599e
kristin-denny <kristingdenny@gmail.com>
2024-08-29 21:23:42 -0400

    Update additional.js

---
+++ Additional/additional.js
@@ -0,0 +1,20 @@
+document.getElementById("No").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    //redirect to landing
+    //window.location.replace("url");
+    console.log("NOOOO!");
+
+
+
+});
+
+document.getElementById("Yes").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    //redirect to add page
+    //window.location.replace("url");
+    console.log("yes pls");
+
+
+});
