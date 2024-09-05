501dd10286e36ce736b6f5f71e2569a944dea00f
kristin-denny <kristingdenny@gmail.com>
2024-09-05 19:37:26 -0400

    updated javascript

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -1,8 +1,7 @@
 const toSearchPage = document.getElementById("Search");
 const toAddPage = document.getElementById("Add");
 const toPrintAllPage = document.getElementById("PrintAll");
-const toAboutPage = document.getElementById("About");
-const toContactPage = document.getElementById("Contact");
+
 
 console.log(toSearchPage);
 
@@ -25,14 +24,4 @@ toPrintAllPage.addEventListener('click', function(event){
     //console.log("print all clicked");
 });
 
-toAboutPage.addEventListener('click', function(event){
-    event.preventDefault();
-   // window.location.assign("url");
-    console.log("about clicked");
-});
 
-toContactPage.addEventListener('click', function(event){
-    event.preventDefault();
-    ///window.location.assign("url");
-    console.log("contact clicked");
-});
