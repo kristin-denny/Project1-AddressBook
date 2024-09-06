a1d6428fa1f4a0eea3e3f6deaa807c2953804289
kristin-denny <kristingdenny@gmail.com>
2024-09-05 20:19:05 -0400

    edited index logic link

--- Welcome/welcome.js
+++ Welcome/welcome.js
@@ -1,9 +1,9 @@
 const toSearchPage = document.getElementById("Search");
 const toAddPage = document.getElementById("Add");
 const toPrintAllPage = document.getElementById("PrintAll");
+const toAboutPage = document.getElementById("About");
+const toContactPage = document.getElementById("Contact");
 
-
-console.log(toSearchPage);
 
 toSearchPage.addEventListener('click', function(event){
     event.preventDefault();
@@ -24,4 +24,14 @@ toPrintAllPage.addEventListener('click', function(event){
     //console.log("print all clicked");
 });
 
+toContactPage.addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.assign("./contact.html");
+    
+});
 
+toAboutPage.addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.assign("./about.html");
+    
+});
