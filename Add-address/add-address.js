31eea23956e1023261051bf4b72b54bb25479e8c
kristin-denny <kristingdenny@gmail.com>
2024-09-05 21:13:18 -0400

    updated links

--- Add-address/add-address.js
+++ Add-address/add-address.js
@@ -26,7 +26,7 @@ document.getElementById("addressEntry").addEventListener("submit", function (eve
 
      localStorage.setItem("addressBook", JSON.stringify(addressBook));
 
-     window.location.assign("Project1-AddressBook/Additional/additional.html")
+     window.location.assign("additional.html")
 
 });
 
